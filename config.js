f7b17c3e0ad90c22cee326841f046691f0ddf545
nguyenhoangthang01103112 <99611221+nguyenhoangthang01103112@users.noreply.github.com>
2022-02-14 01:48:07 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Hà My nè ^^!',
+    introDesc: `anh có điều này muốn nói với em là....`,
+    btnIntro: 'xem tiếp',
+    title: 'Em rất là đáng yêu. Nhưng em sẽ đáng yêu hơn nữa là khi ở bên anh',
+    desc: 'Will you be my Valentine <3 ',
+    btnYes: 'Em đồng ý <3',
+    btnNo: 'Không, Em từ chối',
+    question:'Em có lời gì muốn nói với anh không ?',
+    btnReply: 'Gửi <3',
+    reply: 'Em yêu anh <3',
+    mess: 'Có nhất thiết phải đáng yêu như thế không!',
+    messDesc: 'Mãi luôn là của nhau nhé <3',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/Mhuyenthoaibattan'
+}
